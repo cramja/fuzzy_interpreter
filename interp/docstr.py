df67a5b8c20e1e7f8559428eb933aa3b34fb4400
cramja <marc.spehlmann@gmail.com>
2020-04-26 16:10:52 -0700

    use simple parse scheme

--- interp/docstr.py
+++ interp/docstr.py
@@ -1,50 +1,85 @@
-from lark import Lark
-from lark.indenter import Indenter
+from collections import namedtuple
+from typing import Optional
 
-class TreeIndenter(Indenter):
-    NL_type = '_NL'
-    OPEN_PAREN_types = []
-    CLOSE_PAREN_types = []
-    INDENT_type = '_INDENT'
-    DEDENT_type = '_DEDENT'
-    tab_len = 4
+Token = namedtuple('Token', ['type', 'value'])
+DocStr = namedtuple('DocStr', ['head', 'args'])
 
-GRAMMAR_=r"""
-start: _NL _INDENT body* _DEDENT?
-body: WORD ":" section_body
-   | text_block+
-   | _NL
 
-section_body: _NL [ _INDENT text_block _DEDENT ]
-text_block: (WORD+ _NL)+
-WORD: /[A-Za-z0-9.,;'"`!@$%^&*()]+/
+class LineReader:
+    """
+    Assumes spaces not tabs.
+    """
 
-%import common.WS_INLINE
-%declare _INDENT _DEDENT
-%ignore WS_INLINE
-_NL: /(\r?\n[\t ]*)+/
-"""
+    def __init__(self, s):
+        self.lines = s.split("\n")
+        self.idx = 0
+        self._q = []
 
-GRAMMAR = r"""
-start: (_NL | body)*
+    def __iter__(self):
+        self.idx = 0
+        return self
 
-body: _INDENT statement* _DEDENT
+    def __next__(self):
+        """!
+        RETURN: Token
+        """
+        if self._q:
+            return self._q.pop()
 
-statement: section | line
+        if self.idx < len(self.lines):
+            line = self.lines[self.idx]
+            if line.startswith("!"):
+                self.idx += 1
+                return self.__next__()
 
-section: section_head _NL _INDENT section_body+ _DEDENT 
-section_head: /[A-Z][a-z]+/ ":"
-section_body: line
-    | arg
+            if line == "":
+                self.idx += 1
+                return Token("LINE", "")
 
-arg: WORD+ ":" arg_desc
-arg_desc: WORD* _NL [ _INDENT line* _DEDENT ]
+            stripped = line.strip()
+            self.idx += 1
+            if ":" in stripped:
+                cidx = stripped.rindex(":")
+                l = stripped[:cidx]
+                if cidx + 1 < len(stripped):
+                    self._q.append(Token("LINE", stripped[cidx + 1:].strip()))
+                return Token("ARG", l)
+            return Token("LINE", stripped)
+        else:
+            raise StopIteration
 
-line: WORD* _NL
-WORD: /[A-Za-z0-9.,;'"`!@$%^&*()]+/
 
-%import common.WS_INLINE
-%declare _INDENT _DEDENT
-%ignore WS_INLINE
-_NL: /(\r?\n[\t ]*)+/
-"""
+def parse_doc_string(line) -> Optional[DocStr]:
+    """!
+    Parses a doc string of a particular form. Namely, it must start with !.
+    The next section is called the head and it is a series of lines that
+    must not contain the colon character. The next section is called args, and
+    it looks like
+
+    arg name: This is an arg line. It contains information about the arg.
+        It may continue and if so, it should be indented.
+
+    another arg: This is another arg line. Note that all lines containing colons are
+        treated as start to arg lines. I tried writing a formal grammar for these kinds
+        of strings but realized (after an embarrassing number of hours) that the grammar
+        is not lalr parsable so this simple scheme prevails.
+
+    """
+    lr = LineReader(line)
+    head = []
+    args = {}
+    state = "HEAD"
+    arg_name = None
+    for token in lr:
+        if state == "HEAD" and token.type == "LINE":
+            head.append(token.value)
+        elif state == "HEAD" and token.type == "ARG":
+            state = "ARGS"
+            arg_name = token.value
+            args[arg_name] = []
+        elif state == "ARGS" and token.type == "LINE":
+            args[arg_name].append(token.value)
+        elif state == "ARGS" and token.type == "ARG":
+            arg_name = token.value
+            args[arg_name] = []
+    return DocStr(" ".join(head), {k: " ".join(v) for k, v in args.items()})
--- tests/test_docstr.py
+++ tests/test_docstr.py
@@ -1,59 +1,8 @@
-from lark import Lark
-
-from interp.docstr import TreeIndenter
-from interp.docstr import GRAMMAR
-
-
-def parser(start="start"):
-    return Lark(GRAMMAR, debug=True, start=start, parser='lalr', postlex=TreeIndenter())
+from interp.docstr import parse_doc_string
 
 
 def test_doc_str():
-    """
-    This is a doc string.
+    ds = parse_doc_string(parse_doc_string.__doc__)
+    assert ds.head.startswith("Parses a doc string")
+    assert len(ds.args) == 2
 
-    Args
-        This is the Args section.
-        foo: the first line about foo
-            something else about foo
-
-    Returns
-        This is the note about what is returned
-
-    This is the closing line about the function
-    """
-    p = parser()
-    print(p.parse(test_doc_str.__doc__).pretty())
-
-
-def test_section():
-    p = parser("section")
-    doc_str = """Head:
-    line 1
-    line 2
-    val a: some value
-        some continuation
-    val b: some value
-    line 1
-"""
-    print(p.parse(doc_str).pretty())
-
-
-def test_section_body():
-    p = parser("section_body")
-    section_body_line = "first line\n"
-    print(p.parse(section_body_line).pretty())
-
-    section_body_arg = """first line: line 1
-    line 2
-    line 3
-"""
-    print(p.parse(section_body_arg).pretty())
-
-
-def test_arg():
-    p = parser("arg")
-    tree = p.parse("""name(some val): line one
-    line two
-""")
-    print(tree.pretty())
